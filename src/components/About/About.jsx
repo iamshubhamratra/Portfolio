8d2e4e588caefad2a5bbf1b505c0dbcaec2d4aa9
iamshubhamratra <ratrashubham129@gmail.com>
2025-11-01 01:37:35 +0530

    all

---
+++ src/components/About/About.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/profile2.png';
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Shubham Ratra
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={[
+                'Backend Developer',
+                // 'App Developer',
+                // 'Student',
+                'Coder',
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a Final-year B.Tech (IT) student with a background in backend development. Skilled in building and 
+            optimizing server-side applications, with a strong foundation in programming, databases, and
+            problem-solving. Actively seeking opportunities to learn, grow, and contribute while developing into
+            a well-rounded software professional.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="https://drive.google.com/file/d/1eNdEfabSi6PDcb33Tfqcudbtpxs52qOv/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 1px #845ec5ff, 0 0 1px #7251aaff, 0 0 20px #6b4ca0ff',
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-500 rounded-full"
+            tiltMaxAngleX={10}
+            tiltMaxAngleY={10}
+            perspective={700}
+            scale={1.03}
+            transitionSpeed={800}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Shubham Ratra"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,247 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import ipuLogo from './assets/education_logo/ipu_logo.png';
+import cbseLogo from './assets/education_logo/Cbse-logo.png'
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import yourOsManager from './assets/work_logo/YourOsManager.png';
+
+
+export const SkillsInfo = [
+  // {
+  //   title: 'Frontend',
+  //   skills: [
+  //     // { name: 'HTML', logo: htmlLogo },
+  //     // { name: 'CSS', logo: cssLogo },
+  //     // // { name: 'SASS', logo: sassLogo },
+  //     // { name: 'JavaScript', logo: javascriptLogo },
+  //     // { name: 'React JS', logo: reactjsLogo },
+  //     // // { name: 'Angular', logo: angularLogo },
+  //     // // { name: 'Redux', logo: reduxLogo },
+  //     // // { name: 'Next JS', logo: nextjsLogo }, 
+  //     // { name: 'Tailwind CSS', logo: tailwindcssLogo },
+  //     // // { name: 'GSAP', logo: gsapLogo },
+  //     // // { name: 'Material UI', logo: materialuiLogo },
+  //     // { name: 'Bootstrap', logo: bootstrapLogo },
+  //   ],
+  // },
+  {
+    title: 'Skills',
+    skills: [
+      { name: 'C++', logo: cppLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'Springboot', logo: springbootLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  // {
+  //   title: 'Languages',
+  //   skills: [
+  //     // { name: 'C', logo: cLogo },
+  //     // { name: 'Java', logo: javaLogo },
+  //     // { name: 'Python', logo: pythonLogo },
+  //     // { name: 'C-Sharp', logo: csharpLogo },
+  //     { name: 'JavaScript', logo: javascriptLogo },
+  //   ],
+  // },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      // { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+  //   {
+  //     id: 0,
+  //     img: webverseLogo,
+  //     role: "Fullstack Developer",
+  //     company: "Webverse Digital",
+  //     date: "April 2024 - Present",
+  //     desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "JavaScript",
+  //       "React JS",
+  //       "TypeScript",
+  //       "Node JS",
+  //       "Tailwind CSS",
+  //       "MongoDb",
+  //       "Redux",
+  //       " Next Js",
+  //     ],
+  //   }, 
+  //   {
+  //     id: 1,
+  //     img: agcLogo,
+  //     role: "Fullstack Engineer",
+  //     company: "Agumentik Group of Companies",
+  //     date: "July 2023 - March 2024",
+  //     desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+  //     skills: [
+  //       "ReactJS",
+  //       "Redux",
+  //       "JavaScript",
+  //       "Tailwind CSS",
+  //       "HTML",
+  //       "CSS",
+  //       "SQL",
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     img: newtonschoolLogo,
+  //     role: "Frontend Intern",
+  //     company: "Newton School",
+  //     date: "September 2021 - August 2022",
+  //     desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "Javascript",
+  //       "Bootstrap",
+  //       "Figma",
+  //       "Material UI",
+  //     ],
+  //   },
+  ];
+   
+  export const education = [
+    {
+      id: 0,
+      img: ipuLogo,
+      school: "Dr. Akhilesh Das Gupta Institute of Professional Studies, Delhi",
+      date: "November 2022 - Present",
+      // grade: "7.81 CGPA",
+      desc: "I’m currently pursuing a B.Tech in Information Technology from Dr. Akhilesh Das Gupta Institute of Professional Studies, affiliated with Guru Gobind Singh Indraprastha University. I’m developing a strong foundation in coding, programming, databases, and problem-solving, with a keen interest in building efficient and scalable software solutions.",
+      degree: "Bachelor of Technology - B.Tech",
+    },
+    // {
+    //   id: 1,
+    //   img: bsaLogo,
+    //   school: "BSA College, Mathura",
+    //   date: "Sept 2018 - Aug 2021",
+    //   grade: "73.2%",
+    //   desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    //   degree: "Bachelor of Science - BSC (Computer Science)",
+    // },
+    {
+      id: 2,
+      img: cbseLogo,
+      school: "Sarvodaya Vidyalaya (GBSSS) Dr. Mukherjee nagar, Delhi",
+      date: "2021 - 2022",
+      // grade: "78%",
+      desc: "I completed my class 12 education from Sarvodaya Vidyalaya (GBSSS) Dr. Mukherjee nagar, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "CBSE(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: cbseLogo,
+      school: "Oscar Public School, Delhi",
+      date: "2019 - 2020",
+      // grade: "87.5%",
+      desc: "I completed my class 10 education from Oscar Public School, under the CBSE board.",
+      degree: "CBSE(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Your OS Manager",
+      description:
+        "A modern, secure, and production-ready web-based file and folder management system built with the MERN stack (MongoDB, Express.js, React-inspired frontend, Node.js). This application allows authenticated users to create, organize, and manage folders and files through an intuitive and responsive web interface. Designed for scalability, security, and ease of use.",
+      image: yourOsManager,
+      tags: [ "Node.js", "Express.js", "MongoDB", "HTML", "CSS", "JavaScript", "Bootstrap 5"],
+      github: "https://github.com/iamshubhamratra/YourOsManager.git",
+      webapp: "https://yourosmanager.onrender.com/",
+    },
+    // {
+    //   id: 1,
+    //   title: "CS Prep",
+    //   description:
+    //     "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+    //   image: csprepLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+    //   github: "https://github.com/codingmastr/CSPrep",
+    //   webapp: "https://csprep.netlify.app/",
+    // },
+    // {
+    //   id: 2,
+    //   title: "Movie Recommendation App",
+    //   description:
+    //     "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+    //   image: movierecLogo,
+    //   tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    //   github: "https://github.com/codingmastr/Movie-Recommendation-App",
+    //   webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    // },
+  ];  
